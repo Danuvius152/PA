bc21e64a579305572fe0fba42c2cda4e0efcb3de
Danuvius152 <2064792942@qq.com>
2022-11-15 22:22:24 +0800

    PA2.2

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,17 +4,228 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#define OSTRING_LEN 512
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+void get_parse_out_d(char *p_out, int d){
+	int len = 0, tmp[20];
+	if (d == 0)
+		*p_out++ = '0';
+	
+	else if (d > 0){
+		len = 0;
+		while (d){
+			tmp[len++] = d % 10 + '0';
+			d /= 10;
+		}
+		while (len--)
+			*p_out++ = tmp[len];
+	}
+	else{
+		*p_out++ = '-';
+		d = -d;
+		len = 0;
+		while (d){
+			tmp[len++] = d % 10 + '0';
+			d /= 10;
+		}
+		while (len--)
+			*p_out++ = tmp[len];
+	}
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+void get_parse_out_x(char *p_out, unsigned int d){
+	unsigned int len = 0, tmp[20];//栈-高低位
+	if (d == 0)
+		*p_out++ = '0';
+	else{
+		len = 0;
+		while (d){
+			tmp[len++] = d % 16;
+			d /= 16;
+		}
+		while (len--){
+			if (tmp[len] < 10)
+				*p_out++ = tmp[len] + '0';
+			else
+				*p_out++ = tmp[len] + 'a' - 10;
+    	}
+	}
+}
+
+void get_parse_out_p(char *p_out, unsigned int *pd){
+	int len = 0, tmp[20];
+	unsigned int d = (unsigned int)pd;
+	*p_out++ = '0';
+	*p_out++ = 'x';
+	if (d == 0)
+		*p_out++ = '0';
+	else{
+		len = 0;
+		while (d){
+			tmp[len++] = d % 16;
+			d /= 16;
+		}
+		while (len--){
+			if (tmp[len] < 10)
+				*p_out++ = tmp[len] + '0';
+			else
+				*p_out++ = tmp[len] + 'a' - 10;
+    	}
+	}
+}
+
+int printf(const char *fmt, ...) {
+  char out[OSTRING_LEN];
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsprintf(out, fmt, ap);
+  va_end(ap);
+
+  for (int i = 0; i < num; ++i)
+    putch(out[i]);
+
+  return num;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {//传参中参数类型和个数不确定->vsprintf
+	char *pout = out;
+	size_t i = 0;
+	while(fmt[i] != '\0'){
+		if(fmt[i] != '%'){
+			*(pout++) = fmt[i];//优先级
+		}
+		else{
+			bool is_end = false;
+			char num_leftpad_sym = ' ';//填充
+			bool left_align = false;
+			int width = 0;
+			while(!is_end){
+				switch(fmt[++i]){
+					case '0':{
+						if(fmt[i-1]=='%'){
+							num_leftpad_sym = '0';
+							break;
+						}
+					}
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':{
+						width = width * 10 + fmt[i] - '0';
+						break;
+					}
+					case '-':{
+						if(fmt[i-1] == '%'){
+							left_align = true;
+						}
+						break;
+					}
+					case 's':{
+						const char *str = va_arg(ap, char *);
+						if (!left_align){
+							for (int j = 0; j < width - (int)strlen(str); ++j)
+								*(pout++) = ' ';
+						
+							strcpy(pout, str);
+							pout += strlen(str);
+						}
+						else{
+							strcpy(pout, str);
+							pout += strlen(str);
+							for (int j = 0; j < width - (int)strlen(str); ++j)
+								*(pout++) = ' ';
+						}
+						is_end = true;
+						break;
+					}
+					case 'd':
+					case 'i':{
+						int d = va_arg(ap, int);
+						char buff[20];
+						get_parse_out_d(buff, d);
+						if (!left_align){
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+							strcpy(pout, buff);
+							pout += strlen(buff);
+						}
+						else{
+							strcpy(pout, buff);
+							pout += strlen(buff);
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+						}
+						is_end = true;
+						break;
+					}
+					case 'x':{
+						unsigned int d = va_arg(ap, unsigned int);
+						char buff[20];
+						get_parse_out_x(buff, d);
+						if (!left_align){
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+							strcpy(pout, buff);
+							pout += strlen(buff);
+						}
+						else{
+							strcpy(pout, buff);
+							pout += strlen(buff);
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+						}
+						is_end = true;
+						break;
+					}
+					case 'p':{
+						unsigned int *pd = va_arg(ap, unsigned int*);
+						char buff[20];
+						get_parse_out_p(buff, pd);
+						if (!left_align){
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+							strcpy(pout, buff);
+							pout += strlen(buff);
+						}
+						else{
+							strcpy(pout, buff);
+							pout += strlen(buff);
+							for (int j = 0; j < width - (int)strlen(buff); ++j)
+							*(pout++) = num_leftpad_sym;
+						}
+						is_end = true;
+						break;
+					}
+					case 'c':{
+						char c = (char)va_arg(ap, int);
+						*pout++ = c;
+						break;
+					}
+					default:{
+          				printf("print format is wrong!\n");
+          				assert(0);
+        			}
+				}
+			}
+		}
+		i++;
+	}
+	*pout = '\0';
+	va_end(ap);
+	return strlen(out);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -24,5 +235,6 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
+
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,99 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	size_t len = 0;
+  while(*s++ != '\0')
+    len++;
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t i;
+	for (i = 0; src[i] != '\0'; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+	size_t i;
+	for (i = 0; i < n && (src[i] != '\0'); i++)
+		dst[i] = src[i];
+	for (; i < n; i++)
+		dst[i] = '\0';
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+	size_t pos_dst = strlen(dst);
+  	size_t pos_src = 0;
+  	for(; src[pos_src] != '\0'; pos_src++, pos_dst++)
+    	dst[pos_dst] = src[pos_src];
+  	dst[pos_dst] = '\0';
+  	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	int ret = 0;
+	while ( *s2 && !(ret = *s1 - *s2) ) //相等且没有结束
+		++s1, ++s2;
+	return (ret);
+  // size_t i = 0;
+  // for(; s1[i] != '\0' && s2[i] != '\0'; ++i){
+  //   if(s1[i] > s2[i]) return 1;
+  //   else if(s1[i] == s2[i]) continue;
+  //   else return -1;
+  // }
+  // if(s1[i] == '\0' && s2[i] == '\0') return 0;
+  // else if(s1[i] == '\0') return -1;
+  // else return 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	int ret = 0;
+	while ( *s2 && n-- && !(ret = *s1 - *s2)) //相等且没有结束
+		++s1, ++s2;
+	return (ret);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	for(size_t i = 0; i < n; ++i){
+    	*((char*)s + i) = (char)c;
+	}
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
-}
-
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+	char* p_out = (char*)dst;
+	char* p_in = (char*)src;
+	if(src >= dst)
+	{
+		while(n --) *p_out++ = *p_in++;
+	}
+	else//低地址向高地址拷贝 
+	{
+		p_out += n; p_in += n;
+		while(n --) *p_out-- = *p_in--;
+	}
+	return dst;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+	int ret = 0;
+	char *p_s1 = (char*)s1;
+	char *p_s2 = (char*)s2;
+	while ( n-- && !(ret = *p_s1 - *p_s2) ) //相等且没有结束
+		++p_s1, ++p_s2;
+	return (ret);
 }
+
+void *memcpy(void *out, const void *in, size_t n) {
+	char* p_out = (char*)out;
+	char* p_in = (char*)in;
+	while(n --) *p_out++ = *p_in++;
+	return out;
+}
+
+
 
 #endif
